704fd465550b49999fdf26654aba7a43989f9d5a
Vinícius de Carvalho Pereira <95034279+nonekoy@users.noreply.github.com>
2024-09-29 18:21:40 -0300

    aviso infinito

--- Excell auto messenger/app.py
+++ Excell auto messenger/app.py
@@ -1,6 +1,5 @@
-"""
-a
-"""
+
+
 #   passo a passo de ler os dados das planilhas
 
 #   ler planilha (nome, telefone e mensagem)
@@ -31,10 +30,11 @@ for line in page.iter_rows(min_row=2):#min_row é referente a linha inicial da l
     mensagem = f'{nome} {mensagem}' #formata para primeiro o nome e a mensagem a ser passada
     #mensagem a ser formatada: https://web.whatsapp.com/send?phone=&text=
     mensagemformatada = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
-    # utilizando o quotes, da biblioteca urllib.parse, é possível formatar a mensagem para o padrão do whatsapp web link
-    webbrowser.open(mensagemformatada) # aqui é aberto o link formatado com a mensagem pronta para ser enviada
-    sleep(7)
+
     try:
+        # utilizando o quotes, da biblioteca urllib.parse, é possível formatar a mensagem para o padrão do whatsapp web link
+        webbrowser.open(mensagemformatada) # aqui é aberto o link formatado com a mensagem pronta para ser enviada
+        sleep(10)
         pyautogui.hotkey('enter')
         sleep(2)
         pyautogui.hotkey('ctrl','w')
@@ -43,5 +43,7 @@ for line in page.iter_rows(min_row=2):#min_row é referente a linha inicial da l
         print(f'não foi possível mandar a mensagem para {nome}') 
         #caso uma mensagem não seja passada para alguem, vai ficar salvo no terminal
         erros.append([nome,telefone])
+
+#percebi que o código está rodando infinitamente, ainda estou verificando como parar ao final da lista
 
     
